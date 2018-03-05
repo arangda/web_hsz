f408f91fa007eb7b3a2dc9ddfe267913318e341a
arangda <273890638@qq.com>
2018-03-05 08:55:06 +0800

    201803050855

--- api/config/main.php
+++ api/config/main.php
@@ -14,6 +14,8 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'enableCookieValidation' => true,
+            'cookieValidationKey' => 'your-validation-key',
         ],
         'user' => [
             'identityClass' => 'common\models\Adminuser',
