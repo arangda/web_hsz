f9fb8290cfd70f5e6a49a50acd468df948792f99
xugang <273890638@qq.com>
2018-04-10 18:24:31 +0800

    201804101824

--- api/controllers/WeitaiController.php
+++ api/controllers/WeitaiController.php
@@ -8,9 +8,32 @@ namespace api\controllers;
 
 use yii\data\ActiveDataProvider;
 use yii\rest\ActiveController;
+use yii\filters\Cors;
+use yii\helpers\ArrayHelper;
 
 class WeitaiController extends ActiveController
 {
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'authenticator' => [
+                'class' => QueryParamAuth::className(),
+            ],
+            [
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],//定义允许来源的数组
+                    'Access-Control-Request-Method' => ['GET','POST','PUT','DELETE', 'HEAD', 'OPTIONS'],//允许动作的数组
+                ],
+                'actions' => [
+                    'index' => [
+                        'Access-Control-Allow-Credentials' => true,
+                    ]
+                ]
+            ],
+        ]);
+    }
     public $modelClass = 'common\models\Weitai';
 
 }
