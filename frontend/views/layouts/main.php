d4e8f084bf58c89d7e7c3fa9a47d0e02fd4ff9d0
arangda <273890638@qq.com>
2018-05-29 15:08:55 +0800

    2018052901

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -14,12 +14,13 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>" mip>
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <?= Html::csrfMetaTags() ?>
+    <link rel="stylesheet" type="text/css" href="https://c.mipcdn.com/static/v1/mip.css">
     <title><?= Html::encode($this->title) ?>-西安雁塔友好医院</title>
     <?php $this->head() ?>
 </head>
@@ -76,7 +77,7 @@ AppAsset::register($this);
 
     </div>
 </footer>
-
+<script src="https://c.mipcdn.com/static/v1/mip.js"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
